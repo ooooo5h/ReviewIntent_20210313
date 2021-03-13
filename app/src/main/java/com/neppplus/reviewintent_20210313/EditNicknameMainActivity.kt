f428487a38f81882862a56c5bfc25cd6749bb1ab
전은형 <ooooo5h91@gmail.com>
2021-03-14 08:42:20 +0900

    닉네임 입력 화면 추가

---
+++ app/src/main/java/com/neppplus/reviewintent_20210313/EditNicknameMainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.reviewintent_20210313
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname_main)
+    }
+}

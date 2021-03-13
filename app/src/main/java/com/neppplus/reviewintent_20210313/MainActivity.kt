e10cab3e862f2dd0698f0c3da7cb8734871bf48c
전은형 <ooooo5h91@gmail.com>
2021-03-13 22:47:29 +0900

    넘어갈때 데이터 첨부까지 구현

--- app/src/main/java/com/neppplus/reviewintent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/reviewintent_20210313/MainActivity.kt
@@ -20,16 +20,21 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
-
         moveToSecondBtn.setOnClickListener {
+
+//            어떤 내용이 적혔는지 확인?
+            val inputMessage = messageEdt.text.toString()
+
 //            화면 이동인건 동일 => Intent 기초 사용법은 그대로 적용
 
 //            비행기 티켓팅
             val myIntent = Intent(this, MySecondActivity::class.java)
 
-//            비행기 탑승승
-            startActivty(myIntent)
+//            티켓에 데이터를 추가로 기록, 수하물이 뭔지
+            myIntent.putExtra("message", inputMessage)
+
+//            비행기 탑승
+//            startActivty(myIntent)
 
         }
     }
